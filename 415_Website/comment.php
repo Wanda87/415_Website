cad7d43d56ad54866b074cf1b83222574d73f29a
NicGal-1925 <161859353+NicGal-1925@users.noreply.github.com>
2024-04-01 16:28:11 -0400

    Create comment.php
    In this page the user sees a text box where they can upload their review on the restaurant. This comment is supposed to then be displayed in the comment box.

---
+++ 415_Website/comment.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Share Your Opinion</title>
+
+  
+  <link rel="stylesheet" href = "comment_style.css">
+  </head>
+<body>
+
+<?php
+$servername = 'database-1.ctk6a08mqegz.us-east-2.rds.amazonaws.com';
+$username = 'admin';
+$password = 'password';
+$dbname= 'databaseproject';
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error){
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Check if the form is submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    
+    $comments = $_POST['comments'];
+
+    // Insert the data into the database
+    $sql = "INSERT INTO UserComments (comments) VALUES ('$comments')";
+    if ($conn->query($sql) === TRUE) {
+        // Redirect to the previous page
+        header('Location: rating_comments_log.php');
+        exit();
+    } else {
+        // Display an error message
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+}
+
+$conn->close();
+?>
+
+
+
+  <div class="header">
+    <img src="michelinEatsLogo.png" alt="Logo"> 
+    <h1>Michelin Eats</h1>
+  </div>
+
+  <div class="rating-box">
+    <header>How was your experience?</header>
+    
+    <textarea id="comment-box" placeholder = "Add your comment here..." rows="4"></textarea>
+    <button id="submmit-comment">Submit</button>
+  </div>
+  
+
+</body>
+</html>
+
