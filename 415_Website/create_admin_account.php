8ac3d6754561976bd07a2a6f335796c09d9c77f4
jrodano <jcrodano@outlook.com>
2024-04-01 17:24:45 -0400

    modified:   415_Website/create_admin_account.php 	modified:   415_Website/create_customer_account.php

--- 415_Website/create_admin_account.php
+++ 415_Website/create_admin_account.php
@@ -1,7 +1,43 @@
 <?php
-  // php login authentication stuff will go here
-?>
 
+$servername = "database-1.ctk6a08mqegz.us-east-2.rds.amazonaws.com";
+$username = "admin";
+$password = "password";
+$database = "databaseproject";
+
+$conn = new mysqli($servername, $username, $password, $database);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $user = $_POST['username'];
+    $pass =  $_POST['password'];
+    $confirmPass = $_POST['confirm_password'];
+
+    if ($pass !== $confirmPass) {
+        echo "Passwords do not match";
+    } else {
+        $hashedPass = password_hash($pass, PASSWORD_DEFAULT);
+
+        $stmt = $conn->prepare("INSERT INTO Admins( auser, apass) VALUES ( ?, ?)");
+        $stmt->bind_param("ss",  $user, $hashedPass);
+        $result = $stmt->execute();
+
+        if ($result) {
+            echo "New record created successfully";
+            header("Location: login.php");
+            exit();
+        } else {
+            echo "Error: " . $stmt->error;
+        }
+        $stmt->close();
+    }
+
+}
+
+?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -19,7 +55,7 @@
     </div>
 
     <section class="wrapper-main">
-      <form action="create_admin_account_handler.php" method="post">
+    <form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
         <label for="username">Username:</label>
         <input required type="text" id="username" name="username" placeholder="Enter your username">
         
