0f3f3b87f7206c8d98775688eb40826330549fa0
NIcole <161859353+NicGal-1925@users.noreply.github.com>
2024-04-29 17:53:17 -0400

    Create deny_request.php

---
+++ 415_Website/deny_request.php
@@ -0,0 +1,37 @@
+<?php
+session_start();
+
+$servername = 'databaseprojectrahhhh.ctk6a08mqegz.us-east-2.rds.amazonaws.com';
+$username = 'admin';
+$password = 'password';
+$dbname = 'softwareproject';
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Connection Failed:" . mysqli_connect_error());
+}
+
+// Check if the request ID is provided
+if(isset($_GET['prid'])) {
+  // Delete entry from PendingRestaurant table
+  $entryId = $_GET['prid'];
+  
+  // Prepare statement
+  $stmt = $conn->prepare("DELETE FROM PendingRestaurant WHERE requestId = ?");
+  $stmt->bind_param("i", $entryId); 
+  if ($stmt->execute()) {
+      $_SESSION['deny_message'] = 'Request denied and removed successfully.';
+  } else {
+      $_SESSION['deny_message'] = 'Error denying request: ' . $conn->error;
+  }
+  $stmt->close();
+} else {
+  // No request ID provided
+  $_SESSION['deny_message'] = 'Error: No request ID provided for denial.';
+}
+
+
+// After denial is complete, redirect back to the admin panel page
+header("Location: admin_panel.php");
+exit; // Ensure script execution stops after redirection
+?>
