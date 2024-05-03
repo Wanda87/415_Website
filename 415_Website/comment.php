6e3f182852322298a3939acf0ed317d3463745e9
Showbo92 <showbo92@gmail.com>
2024-05-02 22:26:02 -0400

    modified:   comment.php 	modified:   rating_comments_log.php 	modified:   viewRestaurant.php

--- 415_Website/comment.php
+++ 415_Website/comment.php
@@ -30,10 +30,82 @@
     <header>How was your experience?</header>
     <form id="rating-form" action="rating_comments_log.php" method="post">
       <textarea id="comment-box" name="comment-box" placeholder="Add your comment here..." rows="4"></textarea>
-      <button type="submit">Submit</button>
+      <button type="submit" name = "submit" id = "submit">Submit</button>
     </form>
   </div>
 
+  <?php
+    $currentDirectory = __DIR__;
+    require $currentDirectory . '/vendor/autoload.php';
+    use Aws\Comprehend\ComprehendClient;
+    
+    $cacertPath = 'vendor/cacert.pem'; // Update this with the actual path
+    $servername = 'databaseprojectrahhhh.ctk6a08mqegz.us-east-2.rds.amazonaws.com';
+    $username = 'admin';
+    $password = 'password';
+    $dbname = 'softwareproject';
+    
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
+    if ($conn -> connect_error){
+      die("Connection Failed:" .mysqli_connect_error());
+    }
+    
+    if(isset($_POST['submit'])){
+      $comment = $_POST['comment-box'];
+      $restChoice = $_SESSION['restaurant_choice'];
+      $cid = $_SESSION['cid'];
+
+    
+      $client = new ComprehendClient([
+          'region' => 'us-east-2',
+          'version' => 'latest',
+          'credentials' => [
+              'key' => "AKIAZI2LGBZFJJI6OFTC",
+              'secret' => "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"
+          ],
+          'http' => [
+              'verify' => $cacertPath,
+          ],
+      ]);
+      $result = $client->detectSentiment([
+          'LanguageCode' => 'en',
+          'Text' => $comment,
+      ]);
+      
+      $data = $result->toArray();
+      $sentiment = $data['Sentiment'];
+      
+      $data = $result->toArray();
+      $sentimentScore = $data['SentimentScore'];
+      
+      $weights = [
+          'Positive' => 4,
+          'Negative' => 0.5,
+          'Mixed' => 2,
+          'Neutral' => 3
+      ];
+      
+      $weightedSum = 0;
+      foreach ($weights as $sentiment => $weight) {
+          $weightedSum += $sentimentScore[$sentiment] * $weight;
+      }
+      
+      $maxScore = 0;
+      foreach ($sentimentScore as $score) {
+          $maxScore += $score * max($weights);
+      }
+      
+      $ratingOutOf5 = ($weightedSum / $maxScore) * 5;
+      
+      $sql = "INSERT INTO UserComment (cid, rid, comments, rating) VALUES (?,?,?,?)";
+      $statement = $conn ->prepare($sql);
+      $statement->bind_param("iisd", $cid, $restChoice, $comment, $ratingOutOf5);
+      //$statement->execute();
+      if ($statement->execute()){
+        header("location: rating_comments_log.php");
+      }
+    }
+  ?>
   
 
 </body>
